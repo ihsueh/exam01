794a3c60a0ca4251cb3784258ba802fa3297e653
ihsueh <peter881999@gmail.com>
2020-04-01 17:29:36 +0800

    ver2

--- q2/main.cpp
+++ q2/main.cpp
@@ -2,6 +2,7 @@
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+PwmOut PWM1(D6);
 
 int main()
 {
@@ -13,7 +14,6 @@ int main()
     uLCD.color(BLUE);
     uLCD.locate(0,1);
     uLCD.printf("106062225");
-
     uLCD.line(20, 20, 20, 100, BLACK);
     uLCD.line(20, 100, 100, 100, BLACK);
     uLCD.line(100, 100, 100, 20, BLACK);
@@ -29,4 +29,26 @@ int main()
         uLCD.printf("%2D",i);
         wait(1);
     }*/
+    //while(1){
+        PWM1.period(0.001);
+        PWM1 = 0;
+        bool up = true;
+        while(1){
+            wait(0.1);
+            if(up && PWM1<1){
+                PWM1 = PWM1+0.1;
+            }
+            else if(up && PWM1==1){
+                up = false;
+                PWM1 = PWM1-0,1;
+            }
+            else if(!up && PWM1==0){
+                up = true;
+                PWM1 = PWM1+0.1;
+            }
+            else if(!up && PWM1<1){
+                PWM1 = PWM1-0.1;
+            }
+        }
+    //}
 }
